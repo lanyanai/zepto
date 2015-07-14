d20e9ec3acde575917a72dfa471c28f0533d52e8
zhengdai <zhengdai@foxmail.com>
2015-07-14 11:37:46 +0800

    add comment

--- src/assets.js
+++ src/assets.js
@@ -2,20 +2,25 @@
 //     (c) 2010-2015 Thomas Fuchs
 //     Zepto.js may be freely distributed under the MIT license.
 
-;(function($){
-  var cache = [], timeout
+//重写remove方法 针对img元素ios下移除元素清空内存
+(function($){
+  var cache = [], timeout;
 
   $.fn.remove = function(){
     return this.each(function(){
       if(this.parentNode){
         if(this.tagName === 'IMG'){
-          cache.push(this)
-          this.src = 'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs='
-          if (timeout) clearTimeout(timeout)
-          timeout = setTimeout(function(){ cache = [] }, 60000)
+          cache.push(this);
+          this.src = 'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=';
+          if (timeout) {
+            clearTimeout(timeout);
+          }
+          timeout = setTimeout(function(){
+            cache = [];
+          }, 60000);
         }
-        this.parentNode.removeChild(this)
+        this.parentNode.removeChild(this);
       }
     })
   }
-})(Zepto)
+})(Zepto);
